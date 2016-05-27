052b190385371d0af6fdac6431bdc53c11e6d976
Gorelli <gorelli@maths.ox.ac.uk>
2016-05-27 11:36:40 +0100

    Create blob.h

---
+++ blob.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <vcc.h>
+#include <stdint.h>
+ 
+typedef int BOOL;
+#define TRUE ((int) 1)
+#define FALSE ((int) 0)
+ 
+// An s2n_blob represents an abstract array of uint8_t.
+// It is either derived from a chunk of user-supplied memory, or free (in which case it manages its own).
+// When a derived blob is destroyed, it returns to the owner the memory it was derived from.
+ 
+_(dynamic_owns) struct s2n_blob {
+       // Abstract value
+       _(ghost uint8_t val[size_t])
+       uint32_t size;
+       // true iff the backing memory was provided by the user and is to be returned to him
+       BOOL user_allocated;
+       // if user_allocated, the size and address of the backing memory (but not the data) is part of the abstract value
+       uint32_t allocated;
+       uint8_t *data;
+ 
+       // Coupling invariant
+       _(invariant \forall size_t i; i < size ==> val[i] == data[i])
+      
+       // other data invariants
+       _(invariant size <= allocated)
+       _(invariant allocated ==> \mine(blob_data(\this)))
+       _(invariant user_allocated || !allocated || \malloc_root(blob_data(\this)))
+}s2n_blob;
+_(def \object blob_data(struct s2n_blob *b) { return ((uint8_t[b->allocated]) b->data); })
+#define wrap_blob(b) ghost { \
+       b->val = \lambda size_t i; b->data[i];\
+       b->\owns = b->allocated ? {blob_data(b)} : {}; \
+       _(wrap b) \
+}
+       
+extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
+       _(requires size ==> \extent_mutable(b))
+       _(requires size==> \wrapped((uint8_t[size]) data))
+       _(writes size ? {(uint8_t[size]) data} : {})
+       _(writes \full_extent(b))
+       _(ensures \wrapped(b) && b->size == size && b->allocated == size && b->user_allocated && b->data == data
+              && \forall size_t i; i < size ==> b->val[i] == \old(data[i]))
+       _(ensures \result == 0)
+;
+ 
+extern int s2n_blob_zero(struct s2n_blob *b)
+       _(maintains \wrapped(b))
+       _(writes b)
+       _(ensures \unchanged(b->size) && \unchanged(b->allocated) && \unchanged(b->user_allocated) && \unchanged(b->data))
+       _(ensures \forall size_t i; i < b->size ==> b->val[i] == 0)
+       _(ensures \result == 0)
+;
