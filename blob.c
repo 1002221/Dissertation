3606d2b70bee5df2742b86c79543180bcbd25b87
Gorelli <gorelli@maths.ox.ac.uk>
2016-06-02 17:36:00 +0100

    Update blob.c

--- blob.c
+++ blob.c
@@ -23,7 +23,7 @@ extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
 
 extern int s2n_blob_zero(struct s2n_blob *b)
 {
-    if(b->size && b->data) memcpy(b->data,0,b->size)
+    if(b->size && b->data) memcpy(b->data,0,b->size);
 	b->user_allocated =1;
 	b->mlocked = 0;
 	_(ghost { 
