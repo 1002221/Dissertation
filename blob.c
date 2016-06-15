aa9fd4f86e6b5f29b9d3b6d27ed16c5e8b0b34ec
Gorelli <gorelli@maths.ox.ac.uk>
2016-06-15 19:30:45 +0100

    Update blob.c

--- blob.c
+++ blob.c
@@ -4,20 +4,20 @@
 
 extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
 {
-    b->data = data;
-    b->size = size;
+	b->data = data;
+	b->size = size;
 	b->allocated = size;
 	b->user_allocated =1;
 	b->mlocked = 0;
 	wrap_blob(b);
-    return 0;
+	return 0;
 }
 
 extern int s2n_blob_zero(struct s2n_blob *b)
 {
 	_(unwrap b)
-    if(b->size && b->data) memcpy(b->data,0,b->size);
+	if(b->size && b->data) memcpy(b->data,0,b->size);
 	b->mlocked = 0;
 	wrap_blob(b);
-    return 0;
+	return 0;
 }
