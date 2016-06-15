448a30a37addacb71eb7ae2da7ff2d12a250e253
Gorelli <gorelli@maths.ox.ac.uk>
2016-06-15 19:21:04 +0100

    Update blob.c

--- blob.c
+++ blob.c
@@ -1,10 +1,6 @@
 #include <vcc.h>
-//#include "myerrno.h"
 #include <stdint.h>
-//#include "mysafety.h"
 #include "myblob.h"
-
-//#include "mys2n.h"
 
 extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
 {
@@ -13,11 +9,7 @@ extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
 	b->allocated = size;
 	b->user_allocated =1;
 	b->mlocked = 0;
-	_(ghost { 
-       b->val = \lambda size_t i; b->data[i];
-       b->\owns = b->allocated ? {blob_data(b)} : {}; 
-	   _(wrap b)
-})
+	wrap_blob(b);
     return 0;
 }
 
@@ -26,11 +18,6 @@ extern int s2n_blob_zero(struct s2n_blob *b)
 	_(unwrap b)
     if(b->size && b->data) memcpy(b->data,0,b->size);
 	b->mlocked = 0;
-	_(ghost { 
-       b->val = \lambda size_t i; b->data[i];
-       b->\owns = b->allocated ? {blob_data(b)} : {};
-	   _(assert \inv(b))
-	   _(wrap b)
-})
+	wrap_blob(b);
     return 0;
 }
