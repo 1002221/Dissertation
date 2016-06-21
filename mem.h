0c644b19ded2055b1f7a0a82b61b92a9d6b72e1d
Gorelli <gorelli@maths.ox.ac.uk>
2016-06-21 17:58:39 +0100

    Update mem.h

--- mem.h
+++ mem.h
@@ -22,6 +22,7 @@ int s2n_alloc(struct s2n_blob *b, uint32_t size)
 	_(requires size>0)
 	_(requires \extent_mutable(b))
 	_(requires size < _UI32_MAX - SYSTEM_PAGE_SIZE())
+	_(ensures \result <= 0)
 	_(ensures \result || (\wrapped(b) && b->size == size))
 ;
  
@@ -30,8 +31,8 @@ int s2n_realloc(struct s2n_blob *b, uint32_t size)
 	_(ensures size ==> \wrapped(b))
 	_(requires size < _UI32_MAX - SYSTEM_PAGE_SIZE())
 	_(writes b)
-	//_(ensures \result <= 0)
-	//_(ensures \unchanged(b->user_allocated))
+	_(ensures \result <= 0)
+	_(ensures \unchanged(b->user_allocated))
 	_(ensures \old(b->user_allocated) ==> \unchanged(b->allocated) && \unchanged(b->data))
 	//_(ensures \result ==> size > b->size && \unchanged(b->size) && \unchanged(b->val))
 	_(ensures \result == 0 ==> b->size == size)
@@ -49,81 +50,4 @@ int s2n_free(struct s2n_blob *b)
 	_(ensures !\result ==> b->data == NULL && b->size == 0 && b->allocated == 0)
 ;
 
-int s2n_free(struct s2n_blob *b)
-{
-	int munlock_rc = 0;
-	/*if (b->mlocked) {
-		munlock_rc = munlock(b->data, b->size);
-	}*/
-	_(unwrap b)
-	//if(b->data != NULL) free(b->data);
-	b->data = NULL;
-	b->size = 0;
-	b->allocated = 0;
 
-	/*if (munlock_rc < 0) {
-		S2N_ERROR(S2N_ERR_MUNLOCK);
-	}*/
-	b->mlocked = 0;
-
-	return 0;
-}
-
-int s2n_realloc(struct s2n_blob *b, uint32_t size)
-{
-	if (b->user_allocated) {
-		return -1;
-	}
-	_(unwrap b)
-	if (size == 0) {
-		wrap_blob(b);
-		return s2n_free(b);
-	}
-	if (size < b->allocated && size != 0) {
-		b->size = size;
-		_(assert b->size <= b->allocated)
-		wrap_blob(b);
-		return 0;
-    }
-	uint32_t page_size = 4096;
-	uint32_t allocate = page_size * ((size + (page_size - 1)) / page_size);
-	void *data;
-    /*if (posix_memalign(&data, page_size, allocate)) {
-        S2N_ERROR(S2N_ERR_ALLOC);
-    }*/
-	//posix_memalign(&data, page_size, allocate);
-	if (b->size) {
-		//memcpy_check(data, b->data, b->size);
-		if(b->size && data) memcpy(data,b->data,b->size);
-		//GUARD(s2n_free(b));
-		wrap_blob(b);
-		s2n_free(b);
-	}
-	b->data = data; 
-	b->size = size;
-	b->allocated = allocate;
-	
-	/*#ifdef MADV_DONTDUMP
-		if (madvise(b->data, size, MADV_DONTDUMP) < 0) {
-			GUARD(s2n_free(b));
-			S2N_ERROR(S2N_ERR_MADVISE);
-		}
-	#endif*/
-	/*if (use_mlock == 0) {
-		return 0;
-	}*/
-
-	/*if (mlock(b->data, size) < 0) {
-		GUARD(s2n_free(b));
-		S2N_ERROR(S2N_ERR_MLOCK);
-	}*/
-	b->mlocked = 1;
-	b->user_allocated = 1;
-	_(ghost { 
-		b->val = \lambda size_t i; b->data[i]; 
-		b->\owns = b->allocated ? {blob_data(b)} : {}; 
-		_(assert \inv(b)) 
-	})
-	_(wrap b)
-	return 0;
-}
