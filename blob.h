4fc6801d6a193b9fdb80af76de666ac52fa0368f
Gorelli <gorelli@maths.ox.ac.uk>
2016-08-18 23:29:45 +0100

    Update blob.h

--- blob.h
+++ blob.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vcc.h>
 #include <stdint.h>
-#include <string.h>
+//#include <string.h>
 
 typedef int BOOL;
 #define TRUE ((int) 1)
@@ -23,14 +23,14 @@ _(dynamic_owns) struct s2n_blob {
 	uint8_t *data;
  
 	// Coupling invariant
-	_(invariant \forall size_t i; i < size ==> val[i] == data[i])
+	//_(invariant \forall size_t i; i < size ==> val[i] == data[i])
       
 	// other data invariants
 	_(invariant size <= allocated)
 	_(invariant allocated ==> \mine(blob_data(\this)))
-	_(invariant user_allocated || !allocated || \malloc_root(blob_data(\this))) //if it's not the root object, then the only way it could have been allocated is if it was user-allocated.
+	//_(invariant user_allocated || !allocated || \malloc_root(blob_data(\this))) //if it's not the root object, then the only way it could have been allocated is if it was user-allocated.
 	//_(invariant mlocked == 0)
-	_(invariant size==0 ==> !user_allocated)
+	//_(invariant size==0 ==> !user_allocated)
 }s2n_blob;
 
 _(def \object blob_data(struct s2n_blob *b) { return ((uint8_t[b->allocated]) b->data); })
@@ -43,26 +43,26 @@ _(def \object blob_data(struct s2n_blob *b) { return ((uint8_t[b->allocated]) b-
 })
        
 extern int s2n_blob_init(struct s2n_blob *b, uint8_t *data, uint32_t size)
-	_(requires \extent_mutable(b))
-	_(requires \wrapped((uint8_t[size]) data))
-	_(writes (uint8_t[size]) data)
+	_(requires \mutable(b))
+	//_(requires \wrapped((uint8_t[size]) data))
+    _(writes \array_range(data,size))
+	//_(writes (uint8_t[size]) data)
 	_(requires size>0)
-	_(writes \full_extent(b))
+	_(writes \span(b))
 	_(ensures \wrapped(b) && b->size == size && b->allocated == size && b->user_allocated && b->data == data
 		&& \forall size_t i; i < size ==> b->val[i] == \old(data[i]))
 	_(ensures \result == 0)
 ;
  
 extern int s2n_blob_zero(struct s2n_blob *b)
-	_(maintains \wrapped(b))
-	_(writes b)
-	_(requires b->user_allocated)
+    _(requires \wrapped(b))
+    _(requires b->size > 0 && b->data != NULL)
+    _(requires b->user_allocated)
 	_(requires b->size <= b->allocated)
-	_(ensures \unchanged(b->size))
-	_(ensures \unchanged(b->allocated))
-	_(ensures \unchanged(b->user_allocated))
-	_(ensures \unchanged(b->data))
-	//_(ensures \forall size_t i; i < b->size ==> b->val[i] == 0)
-	_(ensures \result == 0)
+    _(ensures !\result ==> \wrapped(b))
+    _(writes b)
+    _(ensures \unchanged(b->size) && \unchanged(b->allocated) && \unchanged(b->user_allocated) && \unchanged(b->data))
+    //_(ensures !\result ==> \forall size_t i; i < b->size ==> b->val[i] == 0)
+    _(ensures \result <= 0)
 ;
 
