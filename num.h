054367ebba9f5b3a1f748d8f26c91b9972d08af4
Gorelli <gorelli@maths.ox.ac.uk>
2016-08-20 00:42:38 +0100

    Update num.h

--- num.h
+++ num.h
@@ -8,20 +8,20 @@ _(typedef _(record) struct Num {
 
 _(def Num xor(Num n1, Num n2)
 _(requires n1.len == n2.len)
-_(requires valid(n1) && valid(n2))
-_(ensures valid(\result))
+_(requires valid_num(n1) && valid_num(n2))
+_(ensures valid_num(\result))
     {
         return n1 / {.val = (\lambda \natural i; (uint8_t)(i<n1.len? n1.val[i] ^ n2.val[i] : 0))};
     }
 )
 
-_(def \bool valid(Num n1)
+_(def \bool valid_num(Num n1)
 {
-    return (\forall \natural i; i>= n1.len ==> n1.val[i]==(uint8_t)0);
+    return (\forall \natural i; i>= n1.len ==> n1.val[i]==0x0);
 })
 
 _(def \bool xor_comm(Num n1, Num n2)
-_(requires valid(n1) && valid(n2))
+_(requires valid_num(n1) && valid_num(n2))
 _(requires n1.len == n2.len && n2.len)
 _(ensures \result == 1)
 {
@@ -34,36 +34,44 @@ _(ensures \forall \natural i; \result.val[i] == a[i])
 _(ensures \forall \natural i; i>=b ==> a[i]==0)
 {
     Num n;
-    return n/{.val = (\lambda \natural i; i<b? a[i] : (uint8_t)0), .len=b};
+    return n/{.val = (\lambda \natural i; i<b? a[i] : (uint8_t)0x0), .len=b};
 })
 
 _(def Num repeat(uint8_t a, \natural size)
-_(ensures valid(\result))
+_(ensures valid_num(\result))
 {
     Num temp;
-    return temp / {.val = (\lambda \natural i; i<size? a : (uint8_t)0), .len = size} ;
+    return temp / {.val = (\lambda \natural i; i<size? a : (uint8_t)0x0), .len = size} ;
 })
 
 _(def void xor_ass(Num n1, Num n2, Num n3)
-_(requires valid(n1) && valid(n2) && valid(n3))
+_(requires valid_num(n1) && valid_num(n2) && valid_num(n3))
 _(requires n1.len == n2.len && n2.len == n3.len)
 _(ensures xor(xor(n1,n2),n3) == xor(n1,xor(n2,n3)))
 {})
 
 _(def void xor_idempotent(Num n1, uint8_t a)
-_(requires valid(n1))
+_(requires valid_num(n1))
 _(ensures xor(xor(n1,repeat(a,n1.len)),repeat(a,n1.len)) == n1)
 {})
 
 _(def Num concatenate(Num n1, Num n2)
-_(requires valid(n1) && valid(n2))
-_(ensures valid(\result))
+_(requires valid_num(n1) && valid_num(n2))
+_(ensures valid_num(\result))
 {
-    return n1 / {.val = (\lambda \natural i; i<n1.len? n1.val[i] : (i<n1.len+n2.len? n2.val[i-n1.len] : (uint8_t)0)), .len = n1.len+n2.len};
+    return n1 / {.val = (\lambda \natural i; i<n1.len? n1.val[i] : (i<n1.len+n2.len? n2.val[i-n1.len] : (uint8_t)0x0)), .len = n1.len+n2.len};
+})
+
+_(def Num deconcatenate(\natural size, Num n2)
+_(requires valid_num(n2))
+_(requires n2.len >= size)
+_(ensures valid_num(\result))
+{
+    return n2 / {.val = (\lambda \natural i; i<n2.len-size? n2.val[i+size] : (uint8_t)0x0), .len = n2.len-size};
 })
 
 _(def void concatenate_ass(Num n1, Num n2, Num n3)
-_(requires valid(n1) && valid(n2) && valid(n3))
+_(requires valid_num(n1) && valid_num(n2) && valid_num(n3))
 _(ensures concatenate(concatenate(n1,n2),n3) == concatenate(n1, concatenate(n2,n3)))
 {})
 
@@ -72,7 +80,7 @@ _(ensures xor(repeat(a,n),repeat(b,n)) == repeat((uint8_t)(a^b),n)){}
 )
 
 _(def void concatenate_distrib(Num n1, Num n2, Num n3, Num n4)
-_(requires valid(n1) && valid(n2) && valid(n3) && valid(n4))
+_(requires valid_num(n1) && valid_num(n2) && valid_num(n3) && valid_num(n4))
 _(requires n1.len == n3.len && n2.len == n4.len)
 _(ensures xor(concatenate(n1,n2),concatenate(n3,n4)) == concatenate(xor(n1,n3),xor(n2,n4)))
 {})
@@ -82,20 +90,20 @@ _(ensures a^b == (a + b)%2)
 {})
 
 _(def Num num_resize(Num n1, \natural size)
-_(ensures valid(n1))
+_(ensures valid_num(n1))
 {
     if(size <= n1.len)
     {
-        return n1/{.val = (\lambda \natural i; i<size? n1.val[i] : (uint8_t)0), .len = size};
+        return n1/{.val = (\lambda \natural i; i<size? n1.val[i] : (uint8_t)0x0), .len = size};
     }
     else
     {
-        return n1/{.val = (\lambda \natural i; i<n1.len? n1.val[i] : (uint8_t)0), .len = size};
+        return n1/{.val = (\lambda \natural i; i<n1.len? n1.val[i] : (uint8_t)0x0), .len = size};
     }
 })
 
 _(def Num num_segment(Num n1, \natural rear, \natural front, uint8_t a)
-_(maintains valid(n1))
+_(maintains valid_num(n1))
 {
     return n1/{.val = (\lambda \natural i; i>=rear && i< front? a : n1.val[i])};
 })
