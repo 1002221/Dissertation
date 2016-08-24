5a7f540194b6ed5f2e8ccd5c8aaadc359e022140
Gorelli <gorelli@maths.ox.ac.uk>
2016-08-24 15:10:06 +0100

    Update num.h

--- num.h
+++ num.h
@@ -20,18 +20,8 @@ _(def \bool valid_num(Num n1)
     return (\forall \natural i; i>= n1.len ==> n1.val[i]==0x0);
 })
 
-_(def \bool xor_comm(Num n1, Num n2)
-_(requires valid_num(n1) && valid_num(n2))
-_(requires n1.len == n2.len && n2.len)
-_(ensures \result == 1)
-{
-    return (xor(n1,n2) == xor(n2,n1));
-}
-)
-
 _(def Num make_num(uint8_t *a, size_t b)
-_(ensures \forall \natural i; \result.val[i] == a[i])
-_(ensures \forall \natural i; i>=b ==> a[i]==0)
+_(ensures valid_num(\result))
 {
     Num n;
     return n/{.val = (\lambda \natural i; i<b? a[i] : (uint8_t)0x0), .len=b};
@@ -43,17 +33,6 @@ _(ensures valid_num(\result))
     Num temp;
     return temp / {.val = (\lambda \natural i; i<size? a : (uint8_t)0x0), .len = size} ;
 })
-
-_(def void xor_ass(Num n1, Num n2, Num n3)
-_(requires valid_num(n1) && valid_num(n2) && valid_num(n3))
-_(requires n1.len == n2.len && n2.len == n3.len)
-_(ensures xor(xor(n1,n2),n3) == xor(n1,xor(n2,n3)))
-{})
-
-_(def void xor_idempotent(Num n1, uint8_t a)
-_(requires valid_num(n1))
-_(ensures xor(xor(n1,repeat(a,n1.len)),repeat(a,n1.len)) == n1)
-{})
 
 _(def Num concatenate(Num n1, Num n2)
 _(requires valid_num(n1) && valid_num(n2))
@@ -70,27 +49,8 @@ _(ensures valid_num(\result))
     return n2 / {.val = (\lambda \natural i; i<n2.len-size? n2.val[i+size] : (uint8_t)0x0), .len = n2.len-size};
 })
 
-_(def void concatenate_ass(Num n1, Num n2, Num n3)
-_(requires valid_num(n1) && valid_num(n2) && valid_num(n3))
-_(ensures concatenate(concatenate(n1,n2),n3) == concatenate(n1, concatenate(n2,n3)))
-{})
-
-_(def void xor_repeat_distrib(uint8_t a, uint8_t b, \natural n)
-_(ensures xor(repeat(a,n),repeat(b,n)) == repeat((uint8_t)(a^b),n)){}
-)
-
-_(def void concatenate_distrib(Num n1, Num n2, Num n3, Num n4)
-_(requires valid_num(n1) && valid_num(n2) && valid_num(n3) && valid_num(n4))
-_(requires n1.len == n3.len && n2.len == n4.len)
-_(ensures xor(concatenate(n1,n2),concatenate(n3,n4)) == concatenate(xor(n1,n3),xor(n2,n4)))
-{})
-
-_(def void xor_def(uint8_t a, uint8_t b)
-_(ensures a^b == (a + b)%2)
-{})
-
 _(def Num num_resize(Num n1, \natural size)
-_(ensures valid_num(n1))
+_(ensures valid_num(\result))
 {
     if(size <= n1.len)
     {
@@ -102,12 +62,8 @@ _(ensures valid_num(n1))
     }
 })
 
-_(def Num num_segment(Num n1, \natural rear, \natural front, uint8_t a)
-_(maintains valid_num(n1))
+_(def void xor_ass()
+    _(ensures \forall uint8_t a,b,c; (uint8_t)(a^(b^c)) == (uint8_t)((a^b)^c))
 {
-    return n1/{.val = (\lambda \natural i; i>=rear && i< front? a : n1.val[i])};
+    _(assert {:bv} \forall uint8_t a,b,c; _(unchecked)((_(unchecked)(a^b)^c))==_(unchecked)((a^_(unchecked)(b^c))))
 })
-
-_(def void xor_combine(uint8_t a, uint8_t b, Num n)
-_(ensures xor(xor(n,repeat(a,n.len)),repeat(b,n.len)) == xor(n,repeat((uint8_t)(a^b),n.len)))
-{})
