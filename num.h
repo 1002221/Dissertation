5600650d8f5693fed0ee3da14e57d38b48f1c011
Gorelli <gorelli@maths.ox.ac.uk>
2016-08-31 01:01:57 +0100

    Update num.h

--- num.h
+++ num.h
@@ -7,50 +7,50 @@ _(typedef _(record) struct Num {
 }Num;)
 
 _(def Num xor(Num n1, Num n2)
-_(requires n1.len == n2.len)
-_(requires valid_num(n1) && valid_num(n2))
-_(ensures valid_num(\result))
-    {
-        return n1 / {.val = (\lambda \natural i; (uint8_t)(i<n1.len? n1.val[i] ^ n2.val[i] : 0))};
-    }
+    _(requires n1.len == n2.len)
+    _(requires usable_num(n1) && usable_num(n2))
+    _(ensures usable_num(\result))
+{
+    return n1 / {.val = (\lambda \natural i; (uint8_t)(i<n1.len? n1.val[i] ^ n2.val[i] : 0))};
+}
 )
 
-_(def \bool valid_num(Num n1)
+_(def \bool usable_num(Num n1)
 {
     return (\forall \natural i; i>= n1.len ==> n1.val[i]==0x0);
 })
 
 _(def Num make_num(uint8_t *a, size_t b)
-_(ensures valid_num(\result))
+    _(ensures usable_num(\result))
 {
     Num n;
     return n/{.val = (\lambda \natural i; i<b? a[i] : (uint8_t)0x0), .len=b};
 })
 
 _(def Num repeat(uint8_t a, \natural size)
-_(ensures valid_num(\result))
+    _(ensures usable_num(\result))
 {
     Num temp;
     return temp / {.val = (\lambda \natural i; i<size? a : (uint8_t)0x0), .len = size} ;
 })
 
 _(def Num concatenate(Num n1, Num n2)
-_(requires valid_num(n1) && valid_num(n2))
-_(ensures valid_num(\result))
+    _(requires usable_num(n1) && usable_num(n2))
+    _(ensures usable_num(\result))
 {
     return n1 / {.val = (\lambda \natural i; i<n1.len? n1.val[i] : (i<n1.len+n2.len? n2.val[i-n1.len] : (uint8_t)0x0)), .len = n1.len+n2.len};
 })
 
 _(def Num deconcatenate(\natural size, Num n2)
-_(requires valid_num(n2))
-_(requires n2.len >= size)
-_(ensures valid_num(\result))
+    _(requires usable_num(n2))
+    _(requires n2.len >= size)
+    _(ensures usable_num(\result))
 {
     return n2 / {.val = (\lambda \natural i; i<n2.len-size? n2.val[i+size] : (uint8_t)0x0), .len = n2.len-size};
 })
 
 _(def Num num_resize(Num n1, \natural size)
-_(ensures valid_num(\result))
+    _(ensures usable_num(\result))
 {
     if(size <= n1.len)
     {
